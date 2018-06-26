09116b9dd54d02d5d342589d4bf98085b2e7ca1f
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2018-06-26 16:29:44 +0200

    Add polybar launch script

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar -m|sed -e 's/:.*//g')
+do
+    MONITOR=$m polybar -r bottom &
+done
