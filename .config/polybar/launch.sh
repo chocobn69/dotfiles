b662301bc2c28c6152fbcf516e5e6b8f8c698581
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2019-03-30 21:05:51 +0100

    Add a top polybar with todo.txt doing task

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar -m|sed -e 's/:.*//g')
 do
-    MONITOR=$m polybar -r bottom &
+    MONITOR=$m polybar -r bottom -q &
+    MONITOR=$m polybar -r top -q &
 done
