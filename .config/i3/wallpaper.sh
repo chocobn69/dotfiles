ccef1f3a3631f87ed332fae53b09f58c0cbbefb3
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2019-07-11 10:11:09 +0200

    Download only one bg as I only use one monitor

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -1,8 +1,5 @@
 #!/bin/env sh
 rm -f ~/Dropbox/wallpapers/bg-*
-for i in 1 2
-do
-  url=$(curl -s "https://wallhaven.cc/api/v1/search?sorting=random&q=$1" | jq -r '.data[0].path')
-  wget $url -O ~/Dropbox/wallpapers/bg-$i.jpg
-done
+url=$(curl -s "https://wallhaven.cc/api/v1/search?sorting=random&q=$1" | jq -r '.data[0].path')
+wget $url -O ~/Dropbox/wallpapers/bg-1.jpg
 feh --bg-fill ~/Dropbox/wallpapers
