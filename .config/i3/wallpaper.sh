c8420671733bad44151921f63ad7d51fb00f9038
Nicolas Baccelli <nicolas.baccelli@abc-culture.fr>
2021-10-21 16:48:13 +0200

    Update sway config

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -2,4 +2,4 @@
 rm -f ~/Dropbox/wallpapers/bg-*
 url=$(curl -s "https://wallhaven.cc/api/v1/search?sorting=random&q=$1" | jq -r '.data[0].path')
 wget $url -O ~/Dropbox/wallpapers/bg-1.jpg
-feh --bg-fill ~/Dropbox/wallpapers
+swaymsg output HDMI-A-1 bg ~/Dropbox/wallpapers/bg-1.jpg stretch
